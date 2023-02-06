29afdd4f5ba171f298057385580757b230c39e74
Authoma <roberto.simona1@tin.it>
2023-02-06 16:12:58 +0100

    prima stesura

---
+++ led.py
@@ -0,0 +1,33 @@
+'''
+Struttura esempio programma scritto in Python
+
+'''
+
+import time
+import random
+
+def setup():
+    time.sleep(1)
+    print('Fase setup...')
+    time.sleep(1)
+
+def loop():
+    while True:
+        t = random.randint(1, 10)
+        print(t, 'secondi di attesa...')
+        time.sleep(t)
+        print('Fase loop...')
+        time.sleep(1)
+
+def destroy():
+    time.sleep(1)
+    print('Fase destroy...')
+    time.sleep(1)
+
+if __name__ == '__main__':
+    setup()
+    print('Program is starting... \n')
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
